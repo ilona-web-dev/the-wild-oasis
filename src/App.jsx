cb7172066a9edaae99b9e5c8d91d7ec24ec840c9
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-09-03 17:12:15 +0100

    init: create the-wild-oasis basic project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+   return (
+      <>
+         <GlobalStyles />
+         <div>The Wild Oasis</div>
+      </>
+   );
+}
+
+export default App;
