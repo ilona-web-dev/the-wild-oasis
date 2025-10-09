156dd4c94d93f2c3f4013b74ad09ee4b3ee74dc2
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-10-09 15:58:22 +0100

    feat: switch cabin table to compound table primitives

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,23 +1,11 @@
 import styled from 'styled-components';
 import { useState } from 'react';
-
+import Table from '../../ui/Table';
 import CreateCabinForm from './CreateCabinForm';
 import { useDeleteCabin } from './useDeleteCabin';
 import { formatCurrency } from '../../utils/helpers';
 import { HiPencil, HiSquare2Stack, HiTrash } from 'react-icons/hi2';
 import { useCreateCabin } from './useCreateCabin';
-
-const TableRow = styled.div`
-   display: grid;
-   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-   column-gap: 2.4rem;
-   align-items: center;
-   padding: 1.4rem 2.4rem;
-
-   &:not(:last-child) {
-      border-bottom: 1px solid var(--color-grey-100);
-   }
-`;
 
 const Img = styled.img`
    display: block;
@@ -74,7 +62,7 @@ function CabinRow({ cabin }) {
 
    return (
       <>
-         <TableRow role="row">
+         <Table.Row>
             <Img src={image} />
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -97,7 +85,7 @@ function CabinRow({ cabin }) {
                   <HiTrash />
                </button>
             </div>
-         </TableRow>
+         </Table.Row>
          {showForm && <CreateCabinForm cabinToEdit={cabin} />}
       </>
    );
--- src/features/cabins/useCabins.js
+++ src/features/cabins/useCabins.js
@@ -1,15 +1,15 @@
-import { useQuery } from "@tanstack/react-query";
-import { getCabins } from "../../services/apiCabins";
+import { useQuery } from '@tanstack/react-query';
+import { getCabins } from '../../services/apiCabins';
 
 export function useCabins() {
-  const {
-    isLoading,
-    data: cabins,
-    error,
-  } = useQuery({
-    queryKey: ["cabins"],
-    queryFn: getCabins,
-  });
+   const {
+      isPending,
+      data: cabins,
+      error,
+   } = useQuery({
+      queryKey: ['cabins'],
+      queryFn: getCabins,
+   });
 
-  return { isLoading, error, cabins };
+   return { isPending, error, cabins };
 }
