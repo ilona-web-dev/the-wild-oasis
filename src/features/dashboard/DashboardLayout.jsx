51bf69e7d8e4d46b2ce7557d09affb2f0fa09b66
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-01 21:33:35 +0000

    feat: show booking stats on dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -1,19 +1,15 @@
 import styled from 'styled-components';
-
-import DurationChart from 'features/dashboard/DurationChart';
-import SalesChart from 'features/dashboard/SalesChart';
-import Stats from 'features/dashboard/Stats';
-import TodayActivity from 'features/check-in-out/TodayActivity';
-import { useRecentBookings } from 'features/dashboard/useRecentBookings';
-import Spinner from 'ui/Spinner';
+import { useRecentBookings } from './useRecentBookings';
 import { useRecentStays } from './useRecentStays';
-import { useCabins } from 'features/cabins/useCabins';
+import { useCabins } from '../cabins/useCabins';
+import Spinner from '../../ui/Spinner';
+import Stats from './Stats';
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+   display: grid;
+   grid-template-columns: 1fr 1fr 1fr 1fr;
+   grid-template-rows: auto 34rem auto;
+   gap: 2.4rem;
 `;
 
 /*
@@ -23,25 +19,32 @@ We need to distinguish between two types of data here:
 */
 
 function DashboardLayout() {
-  const { isLoading: isLoading1, bookings, numDays } = useRecentBookings();
-  const { isLoading: isLoading2, confirmedStays } = useRecentStays();
-  const { isLoading: isLoading3, cabins } = useCabins();
+   const { bookings, isPending: isPending1 } = useRecentBookings();
 
-  if (isLoading1 || isLoading2 || isLoading3) return <Spinner />;
+   const {
+      stays,
+      isPending: isPending2,
+      confirmedStays,
+      numDays,
+   } = useRecentStays();
 
-  return (
-    <StyledDashboardLayout>
-      <Stats
-        bookings={bookings}
-        confirmedStays={confirmedStays}
-        numDays={numDays}
-        cabinCount={cabins.length}
-      />
-      <TodayActivity />
-      <DurationChart confirmedStays={confirmedStays} />
-      <SalesChart bookings={bookings} numDays={numDays} />
-    </StyledDashboardLayout>
-  );
+   const { cabins, isPending: isPending3 } = useCabins();
+
+   if (isPending1 || isPending2 || isPending3) return <Spinner />;
+
+   return (
+      <StyledDashboardLayout>
+         <Stats
+            bookings={bookings}
+            confirmedStays={confirmedStays}
+            numDays={numDays}
+            cabinCount={cabins.length}
+         />
+         <div>Today's activity</div>
+         <div>Chart stay duration</div>
+         <div>Chart sales</div>
+      </StyledDashboardLayout>
+   );
 }
 
 export default DashboardLayout;
