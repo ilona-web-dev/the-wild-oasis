4be5e17137c381a52cff56d9bd0784fdc2a8aa42
Ilona Mytrokhina <mitrokhina89@gmail.com>
2025-11-05 20:05:07 +0000

    fix: respect capturing option in useOutsideClick

--- src/hooks/useOutsideClick.js
+++ src/hooks/useOutsideClick.js
@@ -1,19 +1,23 @@
 import { useEffect, useRef } from 'react';
 
 export function useOutsideClick(handler, listenCapturing = true) {
-   const ref = useRef(null);
+   const ref = useRef();
 
-   useEffect(() => {
-      function handleClick(e) {
-         if (!ref.current || ref.current.contains(e.target)) return;
-         handler(e);
-      }
+   useEffect(
+      function () {
+         function handleClick(e) {
+            if (ref.current && !ref.current.contains(e.target)) {
+               handler();
+            }
+         }
 
-      document.addEventListener('pointerdown', handleClick, listenCapturing);
+         document.addEventListener('click', handleClick, listenCapturing);
 
-      return () =>
-         document.removeEventListener('pointerdown', handleClick, listenCapturing);
-   }, [handler, listenCapturing]);
+         return () =>
+            document.removeEventListener('click', handleClick, listenCapturing);
+      },
+      [handler, listenCapturing]
+   );
 
    return ref;
 }
